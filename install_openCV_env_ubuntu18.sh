36a4263831aad77167df1aca239e1c076e75555f
unknown <ben.purinton@gmail.com>
2019-02-14 18:31:46 +0100

    clean

--- install_openCV_env_ubuntu18.sh
+++ install_openCV_env_ubuntu18.sh
@@ -1,48 +1,50 @@
-#!/usr/bin/bash
-# Script to build OpenCV 3.4.2 from source into an Anaconda environment
-# SOURCE: https://gist.github.com/clamytoe/3424d0201bba0073ff1313e80ffc6328   
-# Modified by Ben Purinton, December 2018				      
-
-
 # Install requirements
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential cmake unzip pkg-config
-sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libgtk-3-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install python3-dev
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential cmake unzip pkg-config
+sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install libgtk-3-dev
+sudo apt-get -y install libatlas-base-dev gfortran
+sudo apt-get -y install python3-dev
 
-# Move to Downloads folder
+# Move to Downloads folder and get OpenCV source code from GITHUB
 cd ~/Downloads
 wget -O opencv.zip -c https://github.com/opencv/opencv/archive/3.4.2.zip
 wget -O opencv_contrib.zip -c https://github.com/opencv/opencv_contrib/archive/3.4.2.zip
 unzip opencv.zip
 unzip opencv_contrib.zip
 
-# Create virtual environment
-conda create -y --name pebblecounts numpy
+# Create virtual environment called "pebblecounts" with packages (besides OpenCV) installed and activate the environment
+conda create -y --name pebblecounts python=3.6.6 numpy scikit-image scikit-learn gdal scipy matplotlib tk
 conda activate pebblecounts
 
-# Generate make files and install
+# configure opencv with cmake
 cd opencv-3.4.2
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=~/miniconda3/envs/pebblecounts -D INSTALL_PYTHON_EXAMPLES=ON -D INSTALL_C_EXAMPLES=OFF -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.2/modules -D PYTHON_EXECUTABLE=~/miniconda3/envs/pebblecounts/bin/python -D WITH_GTK=ON -D WITH_OPENGL=ON -D WITH_QT=ON -D WITH_TBB=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON ..
+cmake -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX=~/miniconda3/envs/pebblecounts \
+    -DINSTALL_PYTHON_EXAMPLES=ON \
+    -DINSTALL_C_EXAMPLES=OFF \
+    -DOPENCV_EXTRA_MODULES_PATH=~/Downloads/opencv_contrib-3.4.2/modules \
+    -DPYTHON_EXECUTABLE=~/miniconda3/envs/pebblecounts/bin/python \
+    -DBUILD_EXAMPLES=ON ..
+
+# make it! this command might take 15-20 minutes
 make
 make install
-ldconfig -n ~/miniconda3/envs/pebblecounts/lib
 
 # Verify that it worked
 python -c "import cv2; print(cv2.__version__)"
 
+# if this doesn't work, a clean install is done with "conda env remove -n pebblecounts" and "rm -rf build"
+
 # Cleanup
 cd ~/Downloads
-rm opencv.zip opencv_contrib.zip
-rm -rf opencv-3.4.2 opencv_contrib-3.4.2
+rm -rf opencv*
 cd
 
 # Deactivate virtual environment
-conda deactivate
+source deactivate
